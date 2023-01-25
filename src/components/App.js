2edec1a2c0a3be96dbc688e4e2aea2e03a4ac1f6
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 14:00:03 -0500

    init content class

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Content from './Content';
 import Footer from './Footer';
 
 import '../styles/App.css';
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Content />
         <Footer />
       </div>
     );
