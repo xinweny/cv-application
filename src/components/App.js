4ea03b7af5a9a19f9824bdc2d8e2d89bb9486bff
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 13:57:08 -0500

    add basic footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Footer from './Footer';
 
 import '../styles/App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	render() {
+		return (
+			<div className="Footer">
+					<p>Made by <a href="https://github.com/xinweny">xinweny</a> in 2023</p>
+					<img src="#" alt="Github logo" />
+			</div>
+		);
+	}
+}
+
+export default Footer;
