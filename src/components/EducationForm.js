e077be20d5e2974c310764b21107d65ad168805c
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 17:29:16 -0500

    add update functionality to education form

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -4,17 +4,20 @@ import DeleteSectionButton from './DeleteSectionButton';
 
 class EducationForm extends Component {
 	render() {
+		const { id, handleChange } = this.props;
+		const section = 'education';
+
 		return (
 			<div>
 				<form className="education-form">
-					<input type="text" placeholder="Institution name"/>
-					<input type="text" placeholder="Location"/>
-					<input type="text" placeholder="Name of degree/certification"/>
-					<input type="text" placeholder="From"/>
-					<input type="text" placeholder="To"/>
-					<textarea cols="30" rows="3" placeholder="Description"></textarea>
+					<input type="text" placeholder="Institution name" onChange={e => handleChange(section, id, 'name', e.target.value)}/>
+					<input type="text" placeholder="Location" onChange={e => handleChange(section, id, 'location', e.target.value)}/>
+					<input type="text" placeholder="Name of degree/certification" onChange={e => handleChange(section, id, 'cert', e.target.value)}/>
+					<input type="text" placeholder="From" onChange={e => handleChange(section, id, 'from', e.target.value)}/>
+					<input type="text" placeholder="To" onChange={e => handleChange(section, id, 'to', e.target.value)}/>
+					<textarea cols="30" rows="3" placeholder="Description" onChange={e => handleChange(section, id, 'description', e.target.value)}></textarea>
 				</form>
-				<DeleteSectionButton section='education' id={this.props.id} handleClick={this.props.handleBtnClick} />
+				<DeleteSectionButton section={section} id={id} handleClick={this.props.handleBtnClick} />
 			</div>
 		);
 	}
