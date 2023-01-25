5dbb6ac42ff4b0bc9af6320039eb1d31703428af
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 14:48:37 -0500

    init work experience form

--- src/components/CVForm.js
+++ src/components/CVForm.js
@@ -1,11 +1,14 @@
 import { Component } from 'react';
 
 import PersonalDetailsForm from './PersonalDetailsForm';
+import WorkExperienceForm from './WorkExperienceForm';
+
 class CVForm extends Component {
 	render() {
 		return (
 			<div>
 				<PersonalDetailsForm />
+				<WorkExperienceForm />
 			</div>
 		);
 	}
---
+++ src/components/WorkExperienceForm.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+class WorkExperienceForm extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Work Experience</h3>
+				<form className="work-experience-form">
+					<input type="text" placeholder="Position" />
+					<input type="text" placeholder="Company" />
+					<input type="text" placeholder="Location" />
+					<label htmlFor="from">From</label>
+					<input name="from" type="date"/>
+					<label htmlFor="to">To</label>
+					<input name="to" type="date"/>
+					<textarea cols="30" rows="10" placeholder="Job roles"></textarea>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default WorkExperienceForm;
