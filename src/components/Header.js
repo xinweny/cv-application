a5fb8dc5ac08fbe9ffad7baa5a8343e46466164f
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 11:42:47 -0500

    add basic header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="Header">
+					<h1>CV Generator</h1>
+			</div>
+		);
+	}
+}
+
+export default Header;
