c856b284cc93564953ce3e7aafb7ee1dac395ee4
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-25 14:49:25 -0500

    remove spacing

--- src/components/WorkExperienceForm.js
+++ src/components/WorkExperienceForm.js
@@ -6,9 +6,9 @@ class WorkExperienceForm extends Component {
 			<div>
 				<h3>Work Experience</h3>
 				<form className="work-experience-form">
-					<input type="text" placeholder="Position" />
-					<input type="text" placeholder="Company" />
-					<input type="text" placeholder="Location" />
+					<input type="text" placeholder="Position"/>
+					<input type="text" placeholder="Company"/>
+					<input type="text" placeholder="Location"/>
 					<label htmlFor="from">From</label>
 					<input name="from" type="date"/>
 					<label htmlFor="to">To</label>
