794d1bd019f355b268ffbe23f4189324660ff45b
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-26 13:06:24 -0500

    fix warning message bug - form not connected to button

--- src/components/DeleteSectionButton.js
+++ src/components/DeleteSectionButton.js
@@ -3,10 +3,12 @@ import { Component } from 'react';
 import '../styles/DeleteSectionButton.css';
 
 class DeleteSectionButton extends Component {
-
 	render() {
 		return (
-			<button className="delete-button" onClick={() => this.props.handleClick(this.props.section, this.props.id)}>x</button>
+			<button className="delete-button" onClick={event => {
+				event.preventDefault();
+				return this.props.handleClick(this.props.section, this.props.id);
+			}}>x</button>
 		);
 	}
 }
