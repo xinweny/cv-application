3ce5d9712e07401c29a82d077dd41f3da4a7809d
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-30 16:04:57 -0500

    add icons and change header/footer color

--- src/components/AddSectionButton.js
+++ src/components/AddSectionButton.js
@@ -1,11 +1,14 @@
 import { Component } from 'react';
 
 import '../styles/AddSectionButton.css';
+import addBtnIcon from '../assets/add-circle.svg';
 
 class AddSectionButton extends Component {
 	render() {
 		return (
-			<button className="add-section-btn" onClick={this.props.handleClick}>+</button>
+			<button className="add-section-btn" onClick={this.props.handleClick}>
+				<img src={addBtnIcon} alt="Add button" />
+			</button>
 		);
 	}
 }
--- src/components/PersonalSection.js
+++ src/components/PersonalSection.js
@@ -3,6 +3,10 @@ import { Component } from 'react';
 import CVField from './CVField';
 
 import '../styles/PersonalSection.css';
+import phoneIcon from '../assets/phone.svg';
+import addressIcon from '../assets/address.svg';
+import emailIcon from '../assets/email.svg';
+import linkIcon from '../assets/link.svg';
 
 class PersonalSection extends Component {
 	bindArgs(field) {
@@ -20,9 +24,13 @@ class PersonalSection extends Component {
 						<CVField clsName="personal-title" value={info.title} handleChange={this.bindArgs('title')} placeholder="Professional title" />
 					</div>
 					<div className="contact-details">
+						<img src={phoneIcon} alt="Phone Icon" />
 						<CVField clsName="personal-tel" value={info.tel} handleChange={this.bindArgs('tel')} placeholder="Tel no." />
+						<img src={addressIcon} alt="Address Icon" />
 						<CVField clsName="personal-address" value={info.address} handleChange={this.bindArgs('address')} placeholder="Home address" />
+						<img src={emailIcon} alt="Email Icon" />
 						<CVField clsName="personal-email" value={info.email} handleChange={this.bindArgs('email')} placeholder="Email"/>
+						<img src={linkIcon} alt="Link Icon" />
 						<CVField clsName="personal-website" value={info.website} handleChange={this.bindArgs('website')} placeholder="Website URL" />
 					</div>
 				</div>
