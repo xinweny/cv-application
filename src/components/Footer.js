29bd3efa56b33361162be46f3ff3fd3f5a924a8a
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-30 14:47:53 -0500

    add styling and github logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import { Component } from 'react';
 
 import '../styles/Footer.css';
+import githubLogo from '../assets/github.svg';
 
 class Footer extends Component {
 	render() {
 		return (
 			<div className="footer">
 					<p>Made by <a href="https://github.com/xinweny">xinweny</a> in 2023</p>
-					<img src="#" alt="Github logo" />
+					<img src={githubLogo} alt="Github logo" />
 			</div>
 		);
 	}
